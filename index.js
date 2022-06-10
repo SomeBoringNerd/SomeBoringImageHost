d2ee2e3ce71e5da26f7b8e1e9ca78bca35ed3d41
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2022-06-10 10:01:26 +0200

    optimised the code
    No longer save as html
    
    rename files
    
    port is in config file
    
    changed the subfolder

--- index.js
+++ index.js
@@ -5,55 +5,61 @@ const fs = require('fs');
 const express = require('express')
 
 const app = express();
-const port = 8080
 
-app.use('/img', express.static(path.join(__dirname, 'img/')))
-app.use('/host', express.static(path.join(__dirname, 'host/')))
+const config = require('./config.json')
+const sub = '/i'
+const port = config.port
+
+app.use(sub + '/img', express.static(path.join(__dirname, 'img/')))
+app.use(sub + '/css', express.static(path.join(__dirname, 'css/')))
 
 app.listen(port, () => {
     console.log("image host loaded !")
 })
 
-app.post('/host', upload.single('sharex') ,(req, res) => 
+app.post(sub + '/host', upload.single('sharex') ,(req, res) => 
 {
 
     if(req.body.secret != config.img_secret) return res.send('wrong token!');
 
-    let content = '<html>' +
-    '   <head>' +
-    '       <meta name="twitter:card" content="summary_large_image">' +
-    '       <meta property="og:title" content="' + config.img_title + '" />' +
-    '       <meta property="og:type" content="website" />' +
-    '       <meta property="og:site_name" content="' + config.img_site_name + '"/>' +
-    '       <meta property="og:image" content="/app/img/' + req.file.filename + '"/>' +
-    '       <meta property="og:description" content="' + config.img_description + '">' +
-    '       <meta name="theme-color" content="#0C2C63">' +
-    '       <link type="application/json+oembed" href="">' +
-    '       <link rel="stylesheet" href="/upload.css">' +
-    '   </head>' +
-    '   <body>' +
-    '       <center>' +
-    '           <h1>here you go ! :-)</h1>' +
-'               <img src="/app/img/' + req.file.filename + '">' +
-    '           <h3>Honestly, i don\'t think you should open weird links on discord, or anywhere on the internet for what matter</h3>' +
-    '           <h4>made by <a href="https://github.com/SomeBoringNerd/SomeBoringImageHost">SomeBoringNerd</a></h4><br>' +
-    '      </center>' +
-    '   </body>' +
-    '</html>';
+    
 
     let def = generateRandomString(8)
 
-    fs.writeFile(path.join(__dirname, '/host/' + def + '.html'), content, (err) => {
-        if (err)
-          console.log(err);
-    });
+    const oldPath = path.join(__dirname, "/img/" + req.file.filename)  
+    const newPath = path.join(__dirname, "/img/" + def)
+
+    fs.rename(oldPath, newPath, () => console.log('error'))
     
-    res.send("https://someboringnerd.xyz/api/host/" + def);
+    res.send("https://someboringnerd.xyz"+ sub + def);
 })
 
-app.get('/host/:img_id', (req, res) => {
-    if(!fs.existsSync(path.join(__dirname, '/host/' + req.params.img_id + '.html'))) return res.send('Not your lucky day, there\'s nothing here.');
-    res.sendFile(path.join(__dirname, '/host/' + req.params.img_id + '.html'));
+app.get(sub + '/:img_id', (req, res) => {
+    if(!fs.existsSync(path.join(__dirname, '/img/' + req.params.img_id))) return res.send('Not your lucky day, there\'s nothing here.');
+
+    let content = '<html>\n' +
+    '   <head>\n' +
+    '       <meta name="twitter:card" content="summary_large_image">\n' +
+    '       <meta property="og:title" content="' + config.img_title + '" />\n' +
+    '       <meta property="og:type" content="website" />\n' +
+    '       <meta property="og:site_name" content="' + config.img_site_name + '"/>\n' +
+    '       <meta property="og:image" content="/img/' + req.params.img_id + '"/>\n' +
+    '       <meta property="og:description" content="' + config.img_description + '">\n' +
+    '       <meta name="theme-color" content="#0C2C63">\n' +
+    '       <link type="application/json+oembed" href="">\n' +
+    '       <link rel="stylesheet" href="'+ sub +'/css/upload.css">\n' +
+    '   </head>\n' +
+    '   <body>\n' +
+    '       <center>\n' +
+    '           <h1>here you go ! :-)</h1>\n' +
+    '           <img src="'+ sub +'/img/' + req.params.img_id + '">\n' +
+    '           <h3>Honestly, i don\'t think you should open weird links on discord, or anywhere on the internet for what matter</h3>\n' +
+    '           <h4>made by <a href="https://github.com/SomeBoringNerd/SomeBoringImageHost">SomeBoringNerd</a></h4><br>\n' +
+    '      </center>\n' +
+    '   </body>\n' +
+    '</html>\n';
+
+    res.send(content)
 })
 
 const generateRandomString = (myLength) => {
