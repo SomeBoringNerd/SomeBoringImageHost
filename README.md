998b591d854c5da25ff23ce828354beec36ca617
SomeBoringNerd <dev.someboringnerd@protonmail.com>
2023-01-19 11:46:13 +0100

    Added author support, rewrote part of the documentation, added .service file, added links to host-as-a-service for lazy people with money

--- README.md
+++ README.md
@@ -2,7 +2,31 @@
 
 it's a simple image host for sharex meant to be used on discord.
 
-READ THE LICENSE BEFORE USAGE !!
+# notes
+
+you need embed permission to use it.
+
+depending on what url you use (y'know, i-stole-ur.vbucks.com for example) you may need to talk about it with staff.
+
+in my experience, most owners are fine with you using it, but be careful nonetheless.
+
+if you want a "no setup, simple to use" version of that tool, there's a lot of them like :
+
+https://upload.systems (probably the best i've used and have a free tier, it's fast, reliable, and the owners often do giveaways of invitations code)
+
+https://e-z.host/ (that have a terrible reputation, apparently, worked alright for me)
+
+there's a bigger list with reviews here : 
+
+https://bin.fbi.pics/doing_ur_mom/8eGIQZEXeOqaO9f920V4AFGjMQnLTj
+
+That being said, my tool is made for people that already have their VPS and / or domain name, or dont want to pay a third party (even tough that make no sense and if you buy a vps + domain name for that only you'd be better off buying your domain name and give it to upload.systems as they will automatically set it up for you). 
+
+# history 
+
+That tool was made in june of 2022 because a friend skidded my old image host in php and dared me to remake it in javascript.
+
+Took me 4h and that was the last time I was ever productive.
 
 # How to deploy
 
@@ -14,12 +38,20 @@ open a terminal in the folder and type "npm run install"
 
 it will install all the dependancy
 
-open config.json and set everything up (trial and error is a good idea to set up the different values)
+open config.json and set everything up (trial and error is a good idea to set up the different values even if they are documented.)
 
-depending on your OS, use either sharenix.json or sharex.sxcu as your image uploader (I expect you to know how to use one of the two)
-change your token and url if needed
+depending on your OS, use either sharenix.json or sharex.sxcu as your image uploader (I expect you to know how to use one of the two) in the configs folder.
+change your token and url if needed.
 
-open a terminal in the folder and type "node index.js". it is recommanded to use screen, docker, or ng to keep the webserver online.
+last but not least, change the author_name and author_url in static/embed.json for a even more flashy embed.
+
+
+### systemd users : 
+open image.service and change the necessary values (path and users for example), and move it to /etc/systemd/system.
+
+once it's done, to start it, just type `systemctl enable image.service`  and `systemctl start image.service`.
+
+that way, the imagehost will start automatically when the vps start.
 
 # reverse proxy
 
--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const multer  = require('multer')
 const upload = multer({ dest: 'img/' })
 const fs = require('fs');
 const express = require('express')
-const port = config.port
+
+const config = require('./config.json')
+
+const port = config.port;
 
 // variables
 const app = express();
-
-const config = require('./config.json')
 const sub = '/i'
 
 // app.use
 app.use(sub + '/img', express.static(path.join(__dirname, 'img/')))
 app.use(sub + '/css', express.static(path.join(__dirname, 'css/')))
+app.use(sub + '/', express.static(path.join(__dirname, 'static/')))
 
 // start server
 app.listen(port, () => {
@@ -32,7 +34,7 @@ app.post(sub + '/host', upload.single('sharex') ,(req, res) =>
 
     // rename file for a shorter one
     const oldPath = path.join(__dirname, "/img/" + req.file.filename)  
-    const newPath = path.join(__dirname, "/img/" + def)
+    const newPath = path.join(__dirname, "/img/" + def + ".png")
 
     fs.rename(oldPath, newPath, () => {})
     
@@ -43,7 +45,7 @@ app.post(sub + '/host', upload.single('sharex') ,(req, res) =>
 // image's page
 app.get(sub + '/:img_id', (req, res) => {
     // check if file exist
-    if(!fs.existsSync(path.join(__dirname, '/img/' + req.params.img_id))) return res.send('Not your lucky day, there\'s nothing here.');
+    if(!fs.existsSync(path.join(__dirname, '/img/' + req.params.img_id)) && !fs.existsSync(path.join(__dirname, '/img/' + req.params.img_id + ".png"))) return res.send('Not your lucky day, there\'s nothing here.');
 
     let content = `<html>
     <head>
@@ -51,20 +53,28 @@ app.get(sub + '/:img_id', (req, res) => {
         <meta property="og:title" content="${config.img_title}" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="${config.img_site_name}"/>
-        <meta property="og:image" content="${config.url}${sub}/img/${req.params.img_id}"/>
+        <meta property="og:image" content="${config.url}${sub}/img/${req.params.img_id}%EXT%"/>
         <meta property="og:description" content="${config.img_description}">
         <meta name="theme-color" content="#E74C3C">
-        <link rel="stylesheet" href="${sub/css/upload.css}">
+        <link rel="stylesheet" href="${sub}/css/upload.css">
+        <link type="application/json+oembed" href="${config.url}${sub}/embed.json">
     </head>
     <body>
         <center>
             <h1>here you go ! :-)</h1>
-            <img src="${sub}/img/${req.params.img_id}">
-            <h3>Honestly, i don\'t think you should open weird links on discord, or anywhere on the internet for what matter</h3>
+            <img src="${sub}/img/${req.params.img_id}%EXT%">
+            <h3>Honestly, I don\'t think you should open weird links on discord, or anywhere on the internet for what matter</h3>
             <h4>made by <a href="https://github.com/SomeBoringNerd/SomeBoringImageHost">SomeBoringNerd</a></h4><br>
         </center>
     </body>
 </html>`;
+	if(fs.existsSync(path.join(__dirname, '/img/' + req.params.img_id + ".png"))){
+		content = content.replace("%EXT%", ".png");
+		content = content.replace("%EXT%", ".png");
+	}else{
+		content = content.replace("%EXT%", "");
+		content = content.replace("%EXT%", "");
+	}
 
     // send page
     res.send(content)
